fedf03478b9b9fcdc44d75323a8c00db2a9d2dc6
akhil <akhilshalil22@gmail.com>
2022-06-25 09:50:49 +0530

    allowed external host

--- todolist/settings.py
+++ todolist/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(--mj83cxpr45dt@t5+26htwlgf353vpmf&6a+4pdxi6uy49z-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
